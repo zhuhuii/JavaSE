4651fec3d902bc71ee58d31fadf8063c59168bd3
zhuhui <897762690@qq.com>
2020-01-19 16:15:06 +0800

    insert：test

--- datastru/src/abc.java
+++ datastru/src/abc.java
@@ -1,9 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * @Author: zhuhui
  * @Description:
  * @Date: Create in 16:08 2020/1/19
  */
 public class abc {
+    public static void main(String[] args) {
+        System.out.println("我的测试");
+    }
 }
