11478293ca47d0b577dc80151b7f1c6a32e9dfdf
zhuhui <897762690@qq.com>
2020-01-19 16:35:44 +0800

    insert：insert-List

---
+++ List/src/Run.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: zhuhui
+ * @Description:
+ * @Date: Create in 16:34 2020/1/19
+ */
+public class Run {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+    }
+}
